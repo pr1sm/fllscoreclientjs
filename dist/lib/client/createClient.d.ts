537a49e4b9391bd72906afa843b3f9574f24b011
Srinivas Dhanwada <dhanwads@gmail.com>
2018-01-10 00:12:28 -0600

    Regenerate definition files
    This commit deletes and rebuilds the package to remove invalid definition
    files.

--- dist/lib/client/createClient.d.ts
+++
@@ -1,2 +0,0 @@
-import { FLLScoreClient } from '../shared/interface';
-export declare function createWebClient(host?: string, port?: number, name?: string, useWatchdog?: boolean): FLLScoreClient.IWebClient;
--- dist/lib/proxy/createServer.d.ts
+++
@@ -1,4 +0,0 @@
-import { FLLScoreClient } from '../shared/interface';
-import { WebServer } from './webServer';
-export declare function createClient(host?: string, port?: number, name?: string, useWatchdog?: boolean): FLLScoreClient.IClient;
-export declare function createWebServer(host?: string, port?: number, name?: string, useWatchdog?: boolean): WebServer;
